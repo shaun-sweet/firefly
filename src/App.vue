1f8771c1fc4fa7d8eab156c6ce6a508ec31dc6bc
Shaun Sweet <shaun@shaunsweet.com>
2017-10-01 16:26:16 -0700

    Moved navigation into its own component

--- src/App.vue
+++ src/App.vue
@@ -10,47 +10,9 @@
         <span slot="subtitle">Home Automation</span>
       </q-toolbar-title>
     </q-toolbar>
-
     <div slot="left">
-      <q-list no-border link inset-delimiter>
-        <q-list-header>
-          {{ emailAddress }}
-        </q-list-header>
-        <q-select v-model="select" 
-          stack-label="Selected Home"
-          :options="homesList" />
-        <q-side-link item to="/devices">
-          <q-item-side icon="ion-radio-waves" />
-          <q-item-main label="Devices" sublabel="{count w state on?}" />
-        </q-side-link>
-        <q-side-link item to="/routines">
-          <q-item-side icon="ion-clipboard" />
-          <q-item-main label="Routines" sublabel="{or nothing?}" />
-        </q-side-link>
-        <q-side-link item to="/messages">
-          <q-item-side icon="ion-paper-airplane" />
-          <q-item-main label="Messages" />
-        </q-side-link>
-        <q-list-header> Management </q-list-header>
-        <q-side-link item to="/login">
-          <q-item-side item icon="ion-power" />
-          <q-item-main label="Login/Logout" />
-        </q-side-link>
-        <q-side-link item to="/profile">
-          <q-item-side icon="ion-android-person" />
-          <q-item-main label="Profile" />
-        </q-side-link>
-        <q-side-link item to="/home-settings">
-          <q-item-side icon="ion-home" />
-          <q-item-main label="Home Settings" />
-        </q-side-link>
-        <q-side-link item to="/device-settings">
-          <q-item-side icon="ion-android-options" />
-          <q-item-main label="Device Settings" />
-        </q-side-link>
-      </q-list>
+      <navigation :emailAddress="emailAddress" :homesList="homesList" :selectedHome="selectedHome" />
     </div>
-
     <router-view />
 
     <q-toolbar slot="footer">
@@ -61,14 +23,21 @@
 </template>
 
 <script>
+import Navigation from '@/Navigation'
 export default {
   name: 'app',
+  components: {
+    Navigation
+  },
   computed: {
     homesList () {
       return this.$store.getters.homesList
     },
     emailAddress () {
       return this.$store.getters.user.email
+    },
+    selectedHome () {
+      return this.$store.getters.defaultHome[0]
     }
   },
   data () {
---
+++ src/components/Navigation.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-list no-border link inset-delimiter>
+    <q-list-header>
+      {{ emailAddress }}
+    </q-list-header>
+    <q-select v-model="selectedHome" stack-label="Current Home" :options="homesList" />
+    <q-side-link item to="/devices">
+      <q-item-side icon="ion-radio-waves" />
+      <q-item-main label="Devices" sublabel="{count w state on?}" />
+    </q-side-link>
+    <q-side-link item to="/routines">
+      <q-item-side icon="ion-clipboard" />
+      <q-item-main label="Routines" sublabel="{or nothing?}" />
+    </q-side-link>
+    <q-side-link item to="/messages">
+      <q-item-side icon="ion-paper-airplane" />
+      <q-item-main label="Messages" />
+    </q-side-link>
+    <q-list-header> Management </q-list-header>
+    <q-side-link item to="/login">
+      <q-item-side item icon="ion-power" />
+      <q-item-main label="Login/Logout" />
+    </q-side-link>
+    <q-side-link item to="/profile">
+      <q-item-side icon="ion-android-person" />
+      <q-item-main label="Profile" />
+    </q-side-link>
+    <q-side-link item to="/home-settings">
+      <q-item-side icon="ion-home" />
+      <q-item-main label="Home Settings" />
+    </q-side-link>
+    <q-side-link item to="/device-settings">
+      <q-item-side icon="ion-android-options" />
+      <q-item-main label="Device Settings" />
+    </q-side-link>
+  </q-list>
+</template>
+
+<script>
+export default {
+  props: [
+    'homesList',
+    'emailAddress',
+    'selectedHome'
+  ],
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style  lang="stylus" scoped>
+</style>
