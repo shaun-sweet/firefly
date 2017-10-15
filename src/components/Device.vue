c2a1c1df654e42b6a158a168e824b4900adbeba8
Shaun Sweet <shaun@shaunsweet.com>
2017-10-15 16:59:21 -0700

    Change grid layout to 3 device per row

--- src/components/Device.vue
+++ src/components/Device.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">
+  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-4">
     <q-card>
       <q-card-main>
         {{ title }}
