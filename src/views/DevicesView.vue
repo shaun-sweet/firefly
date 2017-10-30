efb34f74667138eb42f1c22f3b6f0be3814089fd
Shaun Sweet <shaun@shaunsweet.com>
2017-10-29 23:40:50 -0700

    Add tabs and x to close

--- src/views/DevicesView.vue
+++ src/views/DevicesView.vue
@@ -2,18 +2,40 @@
   <div class="layout-padding">
     <q-modal 
       class="device-modal"
-      :content-css="{minWidth: '60vw', padding: '1em'}" 
+      :content-css="{minWidth: '60vw'}" 
       @close="closeDeviceMenuHandler()" 
       ref="deviceMenu"
     >
-      <!-- <div class="layout-padding"> -->
-        <action
-          v-for="(action, index) in deviceModalActions"
-          :key="index"
-          :actionMetadata="action"
-         />
-        <q-btn class="close-btn" color="primary" @click="$refs.deviceMenu.close()">Close</q-btn>
-      <!-- </div> -->
+    <q-tabs no-pane-border v-model="selectedTab">
+      <q-tab slot="title" name="actions" label="Actions" default></q-tab>
+      <q-tab slot="title" name="settings" label="Settings" ></q-tab>
+      <q-icon @click="$refs.deviceMenu.close()" class="x-close-button" name="ion-close" />
+      <div class="main-content">
+
+        <q-tab-pane name="actions">
+          <q-list class="actions" no-border>
+            <div class="actions-container">
+              <action
+                v-for="(action, index) in deviceModalActions"
+                :key="index"
+                :actionMetadata="action"
+              />
+              <q-btn class="close-btn" color="primary" @click="$refs.deviceMenu.close()">Close</q-btn>
+            </div>
+            </q-list>
+        </q-tab-pane>
+
+        <q-tab-pane name="settings">
+          <q-list class="settings" no-border>
+            <q-list-header>
+              <h5>Settings</h5>
+            </q-list-header>
+          </q-list>
+        </q-tab-pane>
+      </div>
+    </q-tabs>
+    
+      
     </q-modal>
     <div class="row search-bar">
       <div class="col-6">
@@ -42,9 +64,9 @@
 </template>
 
 <script>
-import Fuse from 'fuse.js'
-import Device from '@/Device'
-import Action from '@/Action'
+import Fuse from "fuse.js";
+import Device from "@/Device";
+import Action from "@/Action";
 
 export default {
   components: {
@@ -52,39 +74,40 @@ export default {
     Action
   },
   computed: {
-    devicesViewList () {
+    devicesViewList() {
       return this.$store.getters.devicesViewList.sort((a, b) => {
-        if (a.alias < b.alias) return -1
-        if (a.alias > b.alias) return 1
-        return 0
-      })
+        if (a.alias < b.alias) return -1;
+        if (a.alias > b.alias) return 1;
+        return 0;
+      });
     },
-    deviceModalActions () {
-      return this.$store.getters.deviceModalActions
+    deviceModalActions() {
+      return this.$store.getters.deviceModalActions;
     }
   },
   methods: {
-    onDeviceMenuHandler (deviceId, deviceMetadata) {
+    onDeviceMenuHandler(deviceId, deviceMetadata) {
       const payload = {
         deviceId,
         deviceMetadata
-      }
-      this.$store.dispatch('openDeviceMenu', payload)
-      this.$refs.deviceMenu.open()
+      };
+      this.$store.dispatch("openDeviceMenu", payload);
+      this.$refs.deviceMenu.open();
     },
-    closeDeviceMenuHandler () {
-      this.$store.dispatch('closeDeviceMenu')
+    closeDeviceMenuHandler() {
+      this.$store.dispatch("closeDeviceMenu");
     },
-    fuzzyFind () {
-      const fuzzyFinder = new Fuse(this.devicesViewList, this.fuzzyFindoptions)
-      return fuzzyFinder.search(this.searchTerms)
+    fuzzyFind() {
+      const fuzzyFinder = new Fuse(this.devicesViewList, this.fuzzyFindoptions);
+      return fuzzyFinder.search(this.searchTerms);
     },
-    selected (item) {
-      return item
+    selected(item) {
+      return item;
     }
   },
-  data () {
+  data() {
     return {
+      selectedTab: '',
       searchTerms: '',
       checked: false,
       fuzzyFindoptions: {
@@ -94,13 +117,11 @@ export default {
         distance: 100,
         maxPatternLength: 32,
         minMatchCharLength: 1,
-        keys: [
-          "alias"
-        ]
+        keys: ['alias']
       }
-    }
+    };
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
@@ -108,7 +129,14 @@ export default {
   margin-left 0.2em
   cursor pointer
 
-.close-btn
+.x-close-button
+  position absolute
+  top 8px
+  right 8px
+  color white
+  cursor pointer
+
+.close-btn 
   margin-left 8px
 
 </style>
