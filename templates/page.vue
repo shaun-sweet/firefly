fdba43ecf7ed4bd21b44af6480e5532c8ea5dc37
Zachary Priddy <github@zpriddy.com>
2017-11-04 22:43:56 -0700

    Update page.vue

--- templates/page.vue
+++ templates/page.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- if you want automatic padding use "layout-padding" class -->
-  <div class="layout-padding">
+  <!-- <div class="layout-padding"> -->
+  <div>
     <!-- your content -->
   </div>
 </template>
