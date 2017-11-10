644a45c782874796beeae24f4ec4ae273dcdede3
Zachary Priddy <github@zpriddy.com>
2017-11-09 22:50:45 -0800

    Added todos

--- src/firebase.js
+++ src/firebase.js
@@ -25,6 +25,7 @@ export const logout = () => firebase.auth().signOut()
 
 export const getCurrentUser = () => firebase.auth().currentUser
 
+// TODO: Usse userData.homes instead of this call.
 export const getUserHomes = (userId) =>
   db.ref(`users/${userId}/homes`).once('value')
 
