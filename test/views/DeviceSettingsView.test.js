7e58531ac51a1e7a24694c13ca23854523109f03
Talal Talhouk <talal@sbcglobal.net>
2017-11-25 10:26:39 -0800

    feat: add tests for buttons

--- test/views/DeviceSettingsView.test.js
+++ test/views/DeviceSettingsView.test.js
@@ -15,8 +15,32 @@ describe('DeviceSettingsView', () => {
     wrapper.vm.handleAddZwave = spy;
     wrapper.update()
     const addButton = wrapper.findAll('q-btn').at(2);
-    // console.log(addButton);
     addButton.trigger('click');
     expect(spy).toHaveBeenCalled();
-  })
+  });
+  it('should call handleRemoveZwave when Add Zwave clicked', () => {
+    const spy = jest.fn();
+    wrapper.vm.handleRemoveZwave = spy;
+    wrapper.update()
+    const removeButton = wrapper.findAll('q-btn').at(3);
+    removeButton.trigger('click');
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should call handleCancelZwave when Add Zwave clicked', () => {
+    const spy = jest.fn();
+    wrapper.vm.handleCancelZwave = spy;
+    wrapper.update()
+    const cancelButton = wrapper.findAll('q-btn').at(4);
+    cancelButton.trigger('click');
+    expect(spy).toHaveBeenCalled();
+  });
+  it('should call handleRepairZwave when Add Zwave clicked', () => {
+    const spy = jest.fn();
+    wrapper.vm.handleRepairZwave = spy;
+    wrapper.update()
+    const repairButton = wrapper.findAll('q-btn').at(5);
+    repairButton.trigger('click');
+    expect(spy).toHaveBeenCalled();
+  });
+
 })
