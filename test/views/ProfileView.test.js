a6399c9d8af21aaa5c30d8071fa4e1ba29bfd26f
Talal Talhouk <talal@sbcglobal.net>
2017-12-15 13:27:46 -0800

    test: update snapshots. Config babelrc test env to fix import syntax

--- test/views/ProfileView.test.js
+++ test/views/ProfileView.test.js
@@ -1,11 +1,27 @@
-import Vue from 'vue';
-import { mount } from 'vue-test-utils';
+import { shallow, createLocalVue } from 'vue-test-utils'
+import Vuex from 'vuex'
 import ProfileView from 'src/views/ProfileView.vue';
+import getters from 'src/store/getters'
+import {initialState, testState} from '../fixtures/state'
+
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
 
 describe('ProfileView', () => {
   let wrapper;
+  let store;
+  let state;
+  let spy;
   beforeEach(() => {
-    wrapper = mount(ProfileView);
+    spy = jest.fn()
+    state = {...testState}
+    store = new Vuex.Store({
+      state,
+      getters
+    })
+    wrapper = shallow(ProfileView,{ store, localVue });
   })
   it('should render the view correctly', () => {
     expect(wrapper.element).toMatchSnapshot();
