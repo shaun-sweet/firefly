ec2450f77b5d52825d60e9ef708d88bcc9362820
Shaun Sweet <shaun@shaunsweet.com>
2018-01-21 19:20:11 -0700

    fix: hotreloading in dev

--- build/script.dev.js
+++ build/script.dev.js
@@ -37,12 +37,12 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
 })
 
 // force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
-  })
-})
+// compiler.plugin('compilation', function (compilation) {
+//   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+//     hotMiddleware.publish({ action: 'reload' })
+//     cb()
+//   })
+// })
 
 // proxy requests like API. See /config/index.js -> dev.proxyTable
 // https://github.com/chimurai/http-proxy-middleware
